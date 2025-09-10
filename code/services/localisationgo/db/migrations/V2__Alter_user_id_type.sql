7d59a9d0439564727b30585517f365ab07871e4e
mithun-hegde-egov <mithun.hegde@egovernments.org>
2025-09-10 13:06:32 +0530

    cleanup

--- code/services/localisationgo/db/migrations/V2__Alter_user_id_type.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE localisation ALTER COLUMN created_by TYPE VARCHAR(256);
-ALTER TABLE localisation ALTER COLUMN last_modified_by TYPE VARCHAR(256); 
--- code/services/localisationgo/internal/repositories/postgres/schema.go
+++
@@ -1,6 +0,0 @@
-package postgres
-
-// GetSeedDataSQL returns SQL to insert initial sample data
-func GetSeedDataSQL() string {
-	return ""
-}
