84afcb2d7378df2a34bf4995d0c004cb707398b5
anishEgov <anish.sharma@egovernments.org>
2025-09-22 11:06:53 +0530

    move main.go to cmd/server

--- code/services/idgen/cmd/server/main.go
+++ code/services/idgen/cmd/server/main.go
@@ -2,39 +2,35 @@ package main
 
 import (
 	"log"
-	"os"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 
 	"idgen/internal"
+	"idgen/internal/config"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// Load .env file
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
-	}
+	// Load configuration
+	cfg := config.LoadConfig()
 
-	// Get Postgres DSN from env
-	dsn := os.Getenv("POSTGRES_DSN")
-	if dsn == "" {
-		log.Fatal("POSTGRES_DSN not set in environment")
-	}
-    log.Printf("connecting");
-	//Initialize DB connection here and pass the handlers    
-	db, err := internal.InitDB(dsn)
+	// Connect to the database
+	db, err := internal.InitDB(cfg)
 	if err != nil {
 		log.Fatalf("DB init failed: %v", err)
 	}
 	defer db.Close()
+
+	// Set Gin mode to release mode by default
+	gin.SetMode(gin.ReleaseMode)
 
 	r := gin.Default()
 
 	r.POST("/template", internal.RegisterTemplateHandler(db))
 	r.POST("/generate", internal.GenerateIdHandler(db))
 
-	log.Println("Starting server on :8080")
-	r.Run(":8080")
-} 
+	port := cfg.Server.RestPort
+	log.Printf("Starting server on port %s...", port)
+	if err := r.Run(":" + port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
